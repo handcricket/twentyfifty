b3affc4b4f405d248918065d84ce03e960969313
Majid <majid@dataguardnxt.com>
2025-01-15 12:38:35 +0530

    fixed

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from decouple import config
 import telepot
 
-import pandas as pd
 import yfinance as yf
-import ta
+import pandas_ta as ta
 
 
 MY_ID = config("MY_ID")
@@ -15,29 +14,28 @@ def check_ema_supertrend(stocks):
     
     for stock in stocks:
         # Fetch 1-hour historical data for the last 5 days to ensure enough data points
-        data = yf.download(stock, interval='1h', period='5d')
-        
-        if len(data) < 50:
-            continue  # Skip if not enough data
+        data = yf.download(stock, interval='1h', period='1mo', group_by='ticker')
+
+        stockData = data[stock]
         
         # Calculate EMA(20) and EMA(50)
-        data['EMA20'] = ta.trend.ema_indicator(data['Close'], window=20)
-        data['EMA50'] = ta.trend.ema_indicator(data['Close'], window=50)
+        stockData['EMA20'] = ta.ema(stockData['Close'], length=20, offset=0)
+        stockData['EMA50'] = ta.ema(stockData['Close'], length=50, offset=0)
         
         # Calculate Supertrend (7,3)
-        supertrend = ta.trend.supertrend(data['High'], data['Low'], data['Close'], length=7, multiplier=3)
-        data['Supertrend'] = supertrend['supertrend']
+        supertrend = ta.supertrend(stockData['High'], stockData['Low'], stockData['Close'], length=10, multiplier=3)
+
+        stockData['Supertrend'] = supertrend['SUPERTd_10_3.0']
+
+        print(stockData)
         
         # Check for EMA crossover and Supertrend condition
-        if data['EMA20'].iloc[-1] > data['EMA50'].iloc[-1] and data['EMA20'].iloc[-2] <= data['EMA50'].iloc[-2]:
-            if data['Supertrend'].iloc[-1] < data['Close'].iloc[-1]:
+        if stockData['EMA20'].iloc[-1] > stockData['EMA50'].iloc[-1] and stockData['EMA20'].iloc[-2] <= stockData['EMA50'].iloc[-2]:
+            if stockData['Supertrend'].iloc[-1] == 1:
                 results.append(stock)
     
     return results
 
-def getSpecials():
-
-    return 'cleanest_specials'
 
 
 def send_telegram_message(msg):
@@ -49,16 +47,18 @@ def send_telegram_message(msg):
 
 if __name__ == "__main__":
 
-    # Get the specials
-    meats = getSpecials()
-
     stocks = ["BIOCON.NS", "BAJFINANCE.NS"]
 
     crosses = check_ema_supertrend(stocks)
 
-    stocksString =  "\n".join(stocks)
     crossesString =  "\n".join(crosses)
+    stocksString =  "\n".join(stocks)
 
-    msg = f"Specials: \n{crossesString} \n\nMeats: \n{meats} \n\nStocks: \n{stocksString}"
+    msg = f"Crosses: \n{crossesString} on {stocksString}"
+
+    print(msg)
+
+    # if len(crosses) > 0:
+    #     send_telegram_message(msg)
 
     send_telegram_message(msg)
